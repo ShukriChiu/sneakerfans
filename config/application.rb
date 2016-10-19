6336c0570d388ffcb29cbca13b6ccb082d051a42
ShukriChiu <shukrichiu@foxmail.com>
2016-10-19 10:41:44 -0400

    add
    Signed-off-by: ShukriChiu <shukrichiu@foxmail.com>

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Sneakerfans
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths << Rails.root.join('app/policies')
+    config.autoload_paths += Dir["#{config.root}/app/policies"]
   end
 end
