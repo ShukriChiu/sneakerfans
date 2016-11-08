a5e72e43d88c13be089f70bac42673781cb2db20
ShukriChiu <shukrichiu@foxmail.com>
2016-11-08 12:06:50 -0500

    remove cookies
    Signed-off-by: ShukriChiu <shukrichiu@foxmail.com>

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::SessionsController < Api::V1::BaseController
   def create
     @user = User.find_by(email: create_params[:email])
     if @user && @user.authenticate(create_params[:password])
-      cookies[:name] = @user.name
-      cookies[:token]= { value: @user.token, expires: 15.minutes.from_now }
       self.current_user = @user
     else
         render :status => 401
